b49b7ba8c53d44118230693607130998e3b7f33f
Paul Tyng <paul@paultyng.net>
2015-11-25 10:25:06 -0500

    Remove `hotpluggable` flag from virtual box 5

--- lib/vagrant-persistent-storage/providers/virtualbox/driver/version_5_0.rb
+++ lib/vagrant-persistent-storage/providers/virtualbox/driver/version_5_0.rb
@@ -3,7 +3,7 @@ module VagrantPlugins
     module Driver
       class Version_5_0
         def attach_storage(location)
-          execute("storageattach", @uuid, "--storagectl", get_controller_name, "--port", "1", "--device", "0", "--type", "hdd", "--medium", "#{location}", "--hotpluggable", "on")
+          execute("storageattach", @uuid, "--storagectl", get_controller_name, "--port", "1", "--device", "0", "--type", "hdd", "--medium", "#{location}")
         end
       end
     end
